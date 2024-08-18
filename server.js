6764effb282b12811fa21ee78aaf2c2cd2628b2d
Priyanshu-maxMan <jaskprince20@gmail.com>
2024-08-18 23:27:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,27 +1,16 @@
-const http = require('https');
-
+const url = 'https://hianime.p.rapidapi.com/anime/episode-srcs?id=steinsgate-3%3Fep%3D230&server=vidstreaming&category=dub';
 const options = {
 	method: 'GET',
-	hostname: 'anime-streaming.p.rapidapi.com',
-	port: null,
-	path: '/latest-anime',
 	headers: {
 		'x-rapidapi-key': '1c1438c1d9msh979fa7f217b0005p186e0fjsn5902434cfcb1',
-		'x-rapidapi-host': 'anime-streaming.p.rapidapi.com'
+		'x-rapidapi-host': 'hianime.p.rapidapi.com'
 	}
 };
 
-const req = http.request(options, function (res) {
-	const chunks = [];
-
-	res.on('data', function (chunk) {
-		chunks.push(chunk);
-	});
-
-	res.on('end', function () {
-		const body = Buffer.concat(chunks);
-		console.log(body.toString());
-	});
-});
-
-req.end();
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
