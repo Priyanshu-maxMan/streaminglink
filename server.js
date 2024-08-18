0544156c6f65ea4777b4cb8f181012e4ff706675
Priyanshu-maxMan <jaskprince20@gmail.com>
2024-08-18 23:18:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const http = require('https');
 
 const options = {
 	method: 'GET',
-	hostname: 'hianime.p.rapidapi.com',
+	hostname: 'anime-streaming.p.rapidapi.com',
 	port: null,
-	path: '/anime/episode-srcs?id=steinsgate-3%3Fep%3D230&server=vidstreaming&category=dub',
+	path: '/latest-anime',
 	headers: {
 		'x-rapidapi-key': '1c1438c1d9msh979fa7f217b0005p186e0fjsn5902434cfcb1',
-		'x-rapidapi-host': 'hianime.p.rapidapi.com'
+		'x-rapidapi-host': 'anime-streaming.p.rapidapi.com'
 	}
 };
 
